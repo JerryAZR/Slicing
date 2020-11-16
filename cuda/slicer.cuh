0dd8d002b5f90a9ab815390902b345e0a0ea8e52
tianhua2 <62573679+tianhua2@users.noreply.github.com>
2020-11-16 01:56:31 -0600

    merge head file

--- cuda/slicer.cuh
+++ cuda/slicer.cuh
@@ -36,4 +36,9 @@ __global__ void fps1(triangle* triangles, size_t num_triangles, char* all_inters
 __global__ void fps2(char* all_intersections, size_t* trunk_length);
 __global__ void fps3(char* sorted_intersections, size_t* trunk_length, bool* out);
 
+__global__ void triangle_sort(triangle* triangles_global, size_t num_triangles, double* zmins_global, int* index_global);
+__global__ void outputArray(triangle* triangles_global, size_t num_triangles, bool* out, int* index_global);
+__device__ int pixelRayIntersectionNew(triangle t, int x, int y);
+__device__ bool getIntersect(int x, int y, triangle* triangles, size_t num_triangles, size_t layer, int* index);
+__device__ void getOutarray(int x, int y, triangle* triangles, size_t num_triangles, size_t layer, size_t outIdx, size_t flagIdx, bool* out, bool* flagArray, int* index);
 #endif
