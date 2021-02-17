6cd02e42327a78a8c9d29ccfdc1d05d41e2737bd
Zerui An <zeruian2@illinois.edu>
2021-02-16 23:10:19 -0600

    bug fix

--- cuda/triangle.cu
+++ cuda/triangle.cu
@@ -65,8 +65,8 @@ void read_stl(string fname, vector <triangle>&v) {
             //populate each point of the triangle
             //using v3::v3(char* bin);
             //Ignore triangles that are parallel to some pixel ray
-            v3 norm(facet);
-            if (norm.z == 0) continue;
+            // v3 norm(facet);
+            // if (norm.z == 0) continue;
             //facet + 12 skips the triangle's unit normal
             v3 p1(facet+12);
             v3 p2(facet+24);
